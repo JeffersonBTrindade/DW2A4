c9ddfecc76a9fb3f524bfe71d8af3ec55d05e548
JeffersonBrits <jeffersonbtrindade@gmail.com>
2022-06-10 18:04:02 -0300

    projeto spa

---
+++ atividades/projeto-spa/server/src/routes.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import nodemailer from 'nodemailer';
+import { NodemailerMailAdapter } from './adapters/nodemailer/nodemailer-mail-adapter';
+import { prisma } from './prisma';
+import { PrismaFeedbacksRepository } from './repositories/prisma/prisma-feedback-repository';
+import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
+
+export const routes = express.Router()
+
+routes.post('/feedbacks', async (req, res) => {
+    const { type, comment, email, screenshot } = req.body;
+
+    const primaFeedbacksRepository = new PrismaFeedbacksRepository()
+    const nodemailerMailAdapter = new NodemailerMailAdapter()
+
+    const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+        primaFeedbacksRepository,
+        nodemailerMailAdapter,
+    )
+
+    await submitFeedbackUseCase.execute({
+        type,
+        comment,
+        email,
+        screenshot,
+    })
+
+    return res.status(201).send()
+});
